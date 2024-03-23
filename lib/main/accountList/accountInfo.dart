bb31085d2855c5aca734367e95136b2d8b342b53
sunjung Oh <sspure1214@naver.com>
2024-03-23 14:23:16 +0900

    mainAccount 화면에서 accountInfo로 넘기기

---
+++ lib/main/accountList/accountInfo.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AccountInfo extends StatefulWidget {
+  const AccountInfo({super.key});
+
+  @override
+  State<AccountInfo> createState() => _AccountInfoState();
+}
+
+class _AccountInfoState extends State<AccountInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
--- lib/main/mainAccount.dart
+++ lib/main/mainAccount.dart
@@ -1,6 +1,8 @@
 //메인 화면 구현
 
 import 'package:flutter/material.dart';
+import 'package:suntown/main/accountList/account.dart';
+import 'package:suntown/main/accountList/accountInfo.dart';
 
 class MainAccount extends StatefulWidget {
   const MainAccount({super.key});
@@ -10,8 +12,30 @@ class MainAccount extends StatefulWidget {
 }
 
 class _MainAccountState extends State<MainAccount> {
+  final page = AccountInfo();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('main')),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('계좌1'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => page
+                    ),
+                );
+              }
+              //dense: true,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
