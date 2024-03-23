dd083d1073049a7e5541f6fb0d81fc7378179ce6
sunjung Oh <sspure1214@naver.com>
2024-03-23 15:16:44 +0900

    InputTransform() - 송금 버튼 구현 완료 - 송금 버튼으로 연결 완료 - 입력창 눌러야 키보드 뜨게끔 설정 완료 - 키보드 숫자만 입력 가능하게 설정 완료

--- lib/main/Exchange/inputTransfor.dart
+++ lib/main/Exchange/inputTransfor.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:suntown/main/Exchange/connectBlue.dart';
+
+//입력창만들고, 키보드 구현
 
 class InputTransform extends StatefulWidget {
   const InputTransform({super.key});
@@ -10,6 +14,44 @@ class InputTransform extends StatefulWidget {
 class _InputTransformState extends State<InputTransform> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector( //키보드 숨기기 적용
+      onTap: () {
+        //FocusManager.instance.primaryFocus?.unfocus();
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // 위젯 넣을 위치
+              TextField(
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(8),
+                  ],
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '액수 입력 칸',
+                  )),
+              //button
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 346,
+                height: 73,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ConnectBlue()));
+                  },
+                  child: Text('송금 하기'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
