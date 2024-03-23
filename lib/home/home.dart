9d967e8e551f691e0fb900f52bcebe49ef59e4d8
sunjung Oh <sspure1214@naver.com>
2024-03-23 13:24:12 +0900

    home 화면에 init를 통해 3초 뒤에 main 화면으로 넘어가는것 구현 완료

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,30 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:suntown/main/mainAccount.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final page = const MainAccount();
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffFFFBD3), //0xff +
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 250,
-                width: 250,
-                child: Lottie.asset("assets/lottie/sun.json"),
-              ),
-              SizedBox(height: 20),
-              Text(
-                '햇살마을',
-                style: TextStyle(fontSize: 55),
-              ),
-            ],
-          ),
+      backgroundColor: const Color(0xffFFFBD3), //0xff +
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 250,
+              width: 250,
+              child: Lottie.asset("assets/lottie/sun.json"),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              '햇살마을',
+              style: TextStyle(fontSize: 55),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
---
+++ lib/main/mainAccount.dart
@@ -0,0 +1,17 @@
+//메인 화면 구현
+
+import 'package:flutter/material.dart';
+
+class MainAccount extends StatefulWidget {
+  const MainAccount({super.key});
+
+  @override
+  State<MainAccount> createState() => _MainAccountState();
+}
+
+class _MainAccountState extends State<MainAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
