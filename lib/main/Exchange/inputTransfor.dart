d38e1d2e99e698429181e125f4450d395e85ac08
sunjung Oh <sspure1214@naver.com>
2024-03-24 16:29:30 +0900

    - inputTransfor 버튼 아래에 고정 완료 - inputTransfor 키보드 올라오면, 버튼 같이 올라오게 설정 완료
    - 이슈 : 말풍선 부분 정렬 문제로 우선 공백 더해둠

--- lib/main/Exchange/inputTransfor.dart
+++ lib/main/Exchange/inputTransfor.dart
@@ -14,38 +14,79 @@ class InputTransform extends StatefulWidget {
 class _InputTransformState extends State<InputTransform> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( //키보드 숨기기 적용
+    return GestureDetector(
+      //키보드 숨기기 적용
       onTap: () {
-        //FocusManager.instance.primaryFocus?.unfocus();
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        body: Center(
+        appBar: AppBar(),
+        body:
+            //입력칸
+            Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // 위젯 넣을 위치
-              TextField(
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                    LengthLimitingTextInputFormatter(8),
-                  ],
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: '액수 입력 칸',
-                  )),
-              //button
-              SizedBox(
-                height: 20,
+              Expanded(
+                flex: 20,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //text
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child :
+                        Column(
+                          children: [
+                            Text(
+                              "얼마를 보낼까요?" + "       ",
+                              style: TextStyle(fontSize: 30),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                              "얼마만큼의 햇살을 보낼지",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            Text(
+                              "입력해주세요!" + "                   ",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 25),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      //입력 창
+                      TextField(
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                          LengthLimitingTextInputFormatter(8),
+                        ],
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: '액수 입력 칸',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
+              // 위젯 넣을 위치
+              //button
+              const Spacer(),
               SizedBox(
                 width: 346,
                 height: 73,
                 child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => ConnectBlue()));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const ConnectBlue()));
                   },
-                  child: Text('송금 하기'),
+                  child: const Text('송금 하기'),
                 ),
               ),
             ],
--- lib/main/Exchange/reciever/permitBluetooth.dart
+++ lib/main/Exchange/reciever/permitBluetooth.dart
@@ -12,61 +12,57 @@ class PermitBluetooth extends StatelessWidget {
       backgroundColor: const Color(0xffFFFBD3), //0xff +
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 50,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 250,
-                      width: 250,
-                      child: Lottie.asset("assets/lottie/bluetooth.json"),
-                    ),
-                    SizedBox(height: 20), //padding
-                    Text(
-                      '블루투스 연결 허용 메세지',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 50,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 250,
+                    width: 250,
+                    child: Lottie.asset("assets/lottie/bluetooth.json"),
+                  ),
+                  SizedBox(height: 20), //padding
+                  Text(
+                    '블루투스 연결 허용 메세지',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
               ),
-              Spacer(),
-              //버튼
-              SizedBox(
-                // SizedBox 대신 Container를 사용 가능
-                width: 346,
-                height: 73,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ChooseAccount()));
-                  },
-                  child: Text('연결 허용'),
-                ),
+            ),
+            Spacer(),
+            //버튼
+            SizedBox(
+              // SizedBox 대신 Container를 사용 가능
+              width: 346,
+              height: 73,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ChooseAccount()));
+                },
+                child: Text('연결 허용'),
               ),
-              SizedBox(height: 20), //padding
-              SizedBox(
-                // SizedBox 대신 Container를 사용 가능
-                width: 346,
-                height: 73,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => RefuseBlueError()));
-                    //원래는 종료가 되어야 하는데, ui 편의상 지금은 refuseBluetoothError로 연결
-                  },
-                  child: Text('연결 거절'),
-                ),
+            ),
+            SizedBox(height: 20), //padding
+            SizedBox(
+              // SizedBox 대신 Container를 사용 가능
+              width: 346,
+              height: 73,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => RefuseBlueError()));
+                  //원래는 종료가 되어야 하는데, ui 편의상 지금은 refuseBluetoothError로 연결
+                },
+                child: Text('연결 거절'),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
