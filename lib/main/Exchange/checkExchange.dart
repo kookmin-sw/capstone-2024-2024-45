44aa8cd6cb669c74f5dd14ffcf0471d8301b211d
sunjung Oh <sspure1214@naver.com>
2024-04-14 17:19:08 +0900

    송금 확인 화면 구현 완료

--- lib/main/Exchange/checkExchange.dart
+++ lib/main/Exchange/checkExchange.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:suntown/main/Exchange/loadingExchange.dart';
-import 'package:suntown/main/alert/exitExchangeAlert.dart';
-import 'package:suntown/main/accountList/accountInfo.dart';
 
+import '../../User/userData.dart';
+import 'inputTransfor.dart';
+
+/*
+송금 확인 화면
+ */
 
 class CheckExchange extends StatefulWidget {
   const CheckExchange({super.key});
@@ -12,6 +17,8 @@ class CheckExchange extends StatefulWidget {
 }
 
 class _CheckExchangeState extends State<CheckExchange> {
+  UserData userData = UserData();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,23 +32,94 @@ class _CheckExchangeState extends State<CheckExchange> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    '뫄뫄 님에게',
-                    style: TextStyle(fontSize: 40),
+                  CircleAvatar(
+                    // 여기에 프로필 이미지 설정
+                    radius: 50, // 이미지 크기 설정
+                    backgroundImage:
+                    NetworkImage(userData.avatar), // 네트워크 이미지 사용 예시
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${userData.lastName}',
+                          style: TextStyle(
+                            color: Color(0xFF4B4A48),
+                            fontSize: 35,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        Text(
+                          '님에게',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xFF4B4A48),
+                            fontSize: 30,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   SizedBox(
                     height: 5,
                   ),
-                  Text(
-                    '1000 햇살을',
-                    style: TextStyle(fontSize: 40),
+                  Container(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Flexible( //넘칠 경우를 대비...거의 없을듯 싶지만 혹시 모르니
+                          child: Text(
+                            '${NumberFormat("#,###").format(userData.amount)}',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFFFF8D4D),
+                              fontSize: 40,
+                              fontFamily: 'Noto Sans KR',
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        Text(
+                          '매듭을',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            color: Color(0xFF4B4A48),
+                            fontSize: 30,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w300,
+                            height: 0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   SizedBox(
                     height: 5,
                   ),
                   Text(
                     '보낼까요?',
-                    style: TextStyle(fontSize: 40),
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      color: Color(0xFF4B4A48),
+                      fontSize: 40,
+                      fontFamily: 'Noto Sans KR',
+                      fontWeight: FontWeight.w300,
+                      height: 0,
+                    ),
                   ),
                 ],
               ),
@@ -57,15 +135,16 @@ class _CheckExchangeState extends State<CheckExchange> {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => LoadingExchange()));
               },
-              child: Text('예, 햇살을 보냅니다.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFF4B4A48),
-                    fontSize: 25,
-                    fontFamily: 'Noto Sans KR',
-                    fontWeight: FontWeight.w500,
-                    height: 0,
-                  ),
+              child: Text(
+                '예, 햇살을 보냅니다.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF4B4A48),
+                  fontSize: 23,
+                  fontFamily: 'Noto Sans KR',
+                  fontWeight: FontWeight.w500,
+                  height: 0,
+                ),
               ),
               style: ElevatedButton.styleFrom(
                 fixedSize: Size(346, 73),
@@ -86,106 +165,20 @@ class _CheckExchangeState extends State<CheckExchange> {
             height: 73,
             child: ElevatedButton(
               onPressed: () {
-                showModalBottomSheet<void>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return SizedBox(
-                      height: 418,
-                      child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              const Text(
-                                '해당 페이지를 벗어나면 햇살을 보낼 수 없어요',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Color(0xFF727272),
-                                  fontSize: 25,
-                                  fontFamily: 'Noto Sans KR',
-                                  fontWeight: FontWeight.w400,
-                                  height: 0,
-                                  letterSpacing: 0.03,
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              const Text(
-                                '송금을 취소할까요?.',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Color(0xFFFF8D4D),
-                                  fontSize: 25,
-                                  fontFamily: 'Noto Sans KR',
-                                  fontWeight: FontWeight.w400,
-                                  height: 0,
-                                  letterSpacing: 0.03,
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              ElevatedButton(
-                                child: const Text(
-                                  '예, 송금을 취소합니다',
-                                  style: TextStyle(
-                                    color: Color(0xFF4B4A48),
-                                    fontSize: 25,
-                                    fontFamily: 'Noto Sans KR',
-                                    fontWeight: FontWeight.w500,
-                                    height: 0,
-                                  ),
-                                ),
-                                onPressed: (){
-                                  Navigator.push(context,
-                                      MaterialPageRoute(builder: (context) => AccountInfo()));
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  fixedSize: Size(346, 73),
-                                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  backgroundColor: Color(0xFFFFD852),
-                                ),
-                              ),
-                              SizedBox(height: 20), // 간견주기 왜 안됨?
-                              ElevatedButton(
-                                child: const Text(
-                                  '아니요, 송금을 계속합니다',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 25,
-                                    fontFamily: 'Noto Sans KR',
-                                    fontWeight: FontWeight.w500,
-                                    height: 0,
-                                  ),
-                                ),
-                                onPressed: () => Navigator.pop(context),
-                                style: ElevatedButton.styleFrom(
-                                  fixedSize: Size(346, 73),
-                                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  backgroundColor: Color(0xFF4B4A48),
-                                ),
-                              ),
-                            ],
-
-                          )
-                      ),
-                    );
-                  },
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => InputTransform()),
                 );
               },
-              child: Text('아니요, 햇살을 보내지 않습니다.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontFamily: 'Noto Sans KR',
-                fontWeight: FontWeight.w500,
-                height: 0,
-              )
-            ),
+              child: Text('보낼 매듭을 잘못 입력했어요!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 23,
+                    fontFamily: 'Noto Sans KR',
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  )),
               style: ElevatedButton.styleFrom(
                 fixedSize: Size(346, 73),
                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
