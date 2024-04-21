5c30d747330488561982c38aec2a0c02f787ef54
sunjung Oh <sspure1214@naver.com>
2024-04-21 13:40:15 +0900

    qr 화면의 메세지 클래스

---
+++ lib/qr/greetingSection.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class GreetingSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "매듭을 받습니다!",
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text.rich(
+            TextSpan(
+              text: '내 ',
+              style: TextStyle(
+                fontSize: 25,
+                color: Color(0xFFFF8D4D),
+                fontFamily: 'Noto Sans KR',
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: '"매듭코드"',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '를',
+                ),
+              ],
+            ),
+          ),
+          Text(
+            "매듭을 받을 이웃에게 보여주세요!",
+            style: TextStyle(
+              fontSize: 25,
+              color: Color(0xFFFF8D4D),
+              fontFamily: 'Noto Sans KR',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
+}
