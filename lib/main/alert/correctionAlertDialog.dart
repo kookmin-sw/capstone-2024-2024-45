3dc65618f098b7dbb8e3f167f2c56db96a56606b
sunjung Oh <sspure1214@naver.com>
2024-04-21 13:38:58 +0900

    각각 유효기간 지난 alert, 송금 수정 dialog 분리 및 구현 완료

---
+++ lib/main/alert/correctionAlertDialog.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:suntown/main/Exchange/inputTransfor.dart';
+import 'package:suntown/qr/qrScanner.dart';
+
+import '../../User/userData.dart';
+
+UserData userData = UserData();
+
+/*
+dialog 추가된 것, 캡스톤에 옮기기, 그리고 exchange 부분 변경사항 변경하기
+ */
+
+class CorrectAlertDialog {
+  static Future<void> show(BuildContext context) async {
+    MediaQueryData mediaQuery = MediaQuery.of(context); //이거 따로 뺄 수 있으면 빼야겠다.
+    double screenWidth = mediaQuery.size.width;
+    double screenHeight = mediaQuery.size.height;
+
+    return await showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Color(0xFFFFF6F6),
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(20.0),
+          height: screenHeight * 0.6,
+          width: screenWidth,
+          child: Column(
+            children: <Widget>[
+              Text(
+                "\u{1F440}",
+                style: TextStyle(fontSize: 30, fontFamily: 'Noto Sans KR'),
+              ),
+              Expanded(
+                flex: 50,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "매듭을 잘못 입력하셨거나",
+                      style: TextStyle(
+                        color: Color(0xFF727272),
+                        fontSize: 23,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    ),
+                    Text("스캔을 잘못하셨나요?",
+                      style: TextStyle(
+                        color: Color(0xFF727272),
+                        fontSize: 23,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("어디로 이동할까요?",
+                      style: TextStyle(
+                        color: Color(0xFFFF8D4D),
+                        fontSize: 23,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),)
+                  ],
+                ),
+              ),
+              Spacer(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => qrScanner()));
+                    },
+                    child: Text(
+                      '매듭 코드 스캔으로 돌아갑니다',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF4B4A48),
+                        fontSize: 23,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(screenWidth*0.9, screenHeight*0.08),
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Color(0xFFFFD852),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  InputTransfor(userId: userData.id)));
+                    },
+                    child: Text(
+                      '보낼 매듭 입력으로 돌아갑니다',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF4B4A48),
+                        fontSize: 23,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(screenWidth*0.9, screenHeight*0.08),
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Color(0xFFFF8D4D),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '"매듭 보내기"를 계속합니다',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(screenWidth*0.9, screenHeight*0.08),
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Color(0xFF4B4A48),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/main/alert/qrTimeOutDialog.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:suntown/main/mainAccount.dart';
+
+class QrTimeOutDialog {
+  static Future<void> showExpiredCodeDialog(
+      BuildContext context, Function() onRetry) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return WillPopScope(
+            onWillPop: () async {
+              onRetry();
+              return true; // true를 반환하여 AlertDialog가 닫히도록 함
+            },
+            child: AlertDialog(
+              backgroundColor: Color(0xFFFFF6F6),
+              contentPadding: EdgeInsets.all(20),
+              content: const Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    "\u{1F62D}",
+                    style: TextStyle(fontSize: 30, fontFamily: 'Noto Sans KR'),
+                  ),
+                  SizedBox(height: 20),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '유효기간',
+                          style: TextStyle(
+                            color: Color(0xFFFF8D4D),
+                            fontSize: 20,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '이 지난 코드에요!\n',
+                          style: TextStyle(
+                            color: Color(0xFFFF8D4D),
+                            fontSize: 20,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '다른 코드를 스캔하려면\n',
+                          style: TextStyle(
+                            color: Color(0xFF4B4A48),
+                            fontSize: 20,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '"다시스캔하기"를\n',
+                          style: TextStyle(
+                            color: Color(0xFFFF8D4D),
+                            fontSize: 20,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '홈으로 돌아가려면\n',
+                          style: TextStyle(
+                            color: Color(0xFF4B4A48),
+                            fontSize: 20,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '"홈으로"를 눌러주세요',
+                          style: TextStyle(
+                            color: Color(0xFFFF8D4D),
+                            fontSize: 20,
+                            fontFamily: 'Noto Sans KR',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              actions: [
+                Row(
+                  children: [
+                    ElevatedButton(
+                      child: const Text(
+                        '홈으로',
+                        style: TextStyle(
+                          color: Color(0xFFFFF6F6),
+                          fontSize: 20,
+                          fontFamily: 'Noto Sans KR',
+                          fontWeight: FontWeight.w500,
+                          height: 0,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop(); // 다이얼로그 닫기
+                        // 메인 화면으로 이동
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => MainAccount()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(130, 60),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        backgroundColor: Color(0xFF4B4A48),
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: const Text(
+                        '다시스캔',
+                        style: TextStyle(
+                          color: Color(0xFF4B4A48),
+                          fontSize: 20,
+                          fontFamily: 'Noto Sans KR',
+                          fontWeight: FontWeight.w500,
+                          height: 0,
+                        ),
+                      ),
+                      onPressed: onRetry,
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(130, 60),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        backgroundColor: Color(0xFFFFD852),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
+  }
+}
