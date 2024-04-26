f44d6c4fd4f5b375aa9b2913194272072b96cbbb
sunjung Oh <sspure1214@naver.com>
2024-04-26 15:58:26 +0900

    home에서 권한 확인 후, 1. 권한이 없으면 권한 설정 화면으로 이동 2. 권한이 있으면 바로 MainAccount(향후 로그인 화면 연결시 로그인 여부 체크 필요)

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:suntown/main/mainAccount.dart';
+
+import '../permission/permissionWidget.dart';
+import '../utils/screenSizeUtil.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,35 +18,75 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final page = const MainAccount();
-
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), () {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+    Timer(const Duration(seconds: 5), () {
+      checkPermissionsAndNavigate();
     });
   }
 
-  @override
+  Future<void> checkPermissionsAndNavigate() async {
+    PermissionStatus cameraPermissionStatus = await Permission.camera.status;
+
+    if (cameraPermissionStatus.isGranted) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MainAccount()));
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => PermissionWidget()));
+    }
+  }
+
   Widget build(BuildContext context) {
+    double screenHeight = ScreenSizeUtil.screenHeight(context);
+    double screenWidth = ScreenSizeUtil.screenWidth(context);
     return Scaffold(
-      backgroundColor: const Color(0xffFFFBD3), //0xff +
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 250,
-              width: 250,
-              child: Lottie.asset("assets/lottie/sun.json"),
+      backgroundColor: const Color(0xffFFFBD3),
+      body: Column(
+        children: [
+          SizedBox(
+            height: screenHeight * 0.3, // Lottie와 텍스트 간격 조절
+          ),
+          Center(
+            child: Lottie.asset(
+              "assets/lottie/house.json",
+              width: screenWidth * 0.5,
+              height: screenWidth * 0.5,
             ),
-            const SizedBox(height: 20),
-            const Text(
-              '매듭 창고',
-              style: TextStyle(fontSize: 55),
+          ),
+          SizedBox(
+            height: screenHeight * 0.05,
+          ),
+          Center(
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: '매듭 ',
+                    style: TextStyle(
+                      color: Color(0xFFFF8D4D),
+                      fontSize: 55,
+                      fontFamily: 'Cafe24 Supermagic OTF',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                      letterSpacing: 0.06,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '창고\n',
+                    style: TextStyle(
+                      color: Color(0xFF4B4A48),
+                      fontSize: 55,
+                      fontFamily: 'Cafe24 Supermagic OTF',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                      letterSpacing: 0.06,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
