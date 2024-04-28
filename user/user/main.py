1e50b1c5cf07759a8d3218fa9bb1af5b10c47a0e
IngenieurSong <ingeieur.song@gmail.com>
2024-04-28 16:20:05 +0900

    [FEAT] main 함수 추가

--- user/user/main.py
+++ user/user/main.py
@@ -1,5 +1,19 @@
 """ user service의 메인 클라이언트
 """
 from fastapi import FastAPI
+import os
+from sqlmodel import SQLModel, create_engine
+
+from routers import user_router
 
 app = FastAPI(version = "0.0.1")
+
+app.include_router(user_router)
+
+@app.on_event("startup")
+def on_startup():
+    # database_file = os.environ["DB_NAME"]
+    database_connection_string = "sqlite:///user-test.db"
+    engine_url = create_engine(database_connection_string, echo = True)
+
+    SQLModel.metadata.create_all(engine_url)
