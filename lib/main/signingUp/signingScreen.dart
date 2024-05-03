46d635de8f90ce5f4c52151d48758a7dd5c8187e
Heo-jieun <jieunheo192@gmail.com>
2024-05-03 20:09:35 +0900

    카카오로그인 파이어베이스 연동 완료

--- lib/main/signingUp/signingScreen.dart
+++ lib/main/signingUp/signingScreen.dart
@@ -40,61 +40,61 @@ class _signingUPState extends State<signingUP> {
                         //login 되고 안되고에 따라 새로운 stream이 들어옴.
                         stream: FirebaseAuth.instance.authStateChanges(),
                         builder: (context, snapshot) {
-                          // login이 안된 경우
-                          if (!snapshot.hasData){
-                            return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    height: 75,
-                                  ),
-                                  Text(
-                                    '${viewModel.isLogined}',
-                                    style : Theme.of(context).textTheme.headline4,
-                                  ),
-                                  SizedBox(
-                                    width: 343,
-                                    height : 72,
-                                    child: Text(
-                                      '로그인하고\n매듭창고 시작하기.',
-                                      style: TextStyle(
-                                        color: Color(0xFF4B4A48),
-                                        fontSize: screenWidth * 0.06,
-                                        fontFamily: 'Noto Sans KR',
-                                        fontWeight: FontWeight.w700,
-                                        height: 0,
-                                      ),
+                          // login이 진행된 경우
+                          if (snapshot.hasData){
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) => defaultAccount()));
+                          }
+                          return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 75,
+                                ),
+                                Text(
+                                  '${viewModel.isLogined}',
+                                  style : Theme.of(context).textTheme.headline4,
+                                ),
+                                SizedBox(
+                                  width: 343,
+                                  height : 72,
+                                  child: Text(
+                                    '로그인하고\n매듭창고 시작하기.',
+                                    style: TextStyle(
+                                      color: Color(0xFF4B4A48),
+                                      fontSize: screenWidth * 0.06,
+                                      fontFamily: 'Noto Sans KR',
+                                      fontWeight: FontWeight.w700,
+                                      height: 0,
                                     ),
                                   ),
-                                  SizedBox(
-                                    height: 236,
-                                  ),
-                                  InkWell(
-                                    onTap: () {
-                                      viewModel.login();
-                                      //화면 갱신
-                                      setState(() {});
-                                      print("카카오 로그인");
-                                    },
-                                    child: Image.asset("assets/images/kakao_login_large_wide.png"),
-                                  ),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  InkWell(
-                                      onTap: () {
-                                        signInWithGoogle();
-                                        //화면 갱신
-                                        setState(() {});
-                                        print("구글 로그인");
-                                      },
-                                      child: Image.asset('assets/images/google_login_wide.png')
-                                  ),
-                                ]
-                            );
-                          }
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => defaultAccount()));
+                                ),
+                                SizedBox(
+                                  height: 236,
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    viewModel.login();
+                                    //화면 갱신
+                                    setState(() {});
+                                    print("카카오 로그인");
+                                  },
+                                  child: Image.asset("assets/images/kakao_login_large_wide.png"),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    signInWithGoogle();
+                                    //화면 갱신
+                                    setState(() {});
+                                    print("구글 로그인");
+                                  },
+                                  child: Image.asset('assets/images/google_login_wide.png')
+                                ),
+                              ]
+                          );
                         }
                       )
                   )
