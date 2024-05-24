6f8481470afb772ad6c81cb5539e266135285c95
Heo-jieun <jieunheo192@gmail.com>
2024-05-24 13:09:31 +0900

    Home에 로그인 섹션 관리 적용

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:suntown/main/defaultAccount.dart';
 import 'package:suntown/main/mainAccount.dart';
-import 'package:suntown/main/manage/accountInfoManage.dart';
-import 'package:suntown/main/manage/userInfoManage.dart';
 import 'package:suntown/main/signingUp/signingScreen.dart';
+import 'package:suntown/utils/api/connect/tokenRefreashPost.dart';
+import 'package:suntown/utils/api/connect/tokenReissuePost.dart';
 
 import '../permission/permissionWidget.dart';
 import '../utils/screenSizeUtil.dart';
@@ -31,47 +32,81 @@ class _HomeState extends State<Home> {
       checkPermissionsAndNavigate();
     });
   }
-
+  // 권한 허용 확인
   Future<void> checkPermissionsAndNavigate() async {
     PermissionStatus cameraPermissionStatus = await Permission.camera.status;
-
     if (cameraPermissionStatus.isGranted) { // 카메라 권한이 설정 되어있는지 확인
-      final token = await secureStorage.read(key: 'kakaoToken');
-      if (token != null) { // 로그인된 회원이 있으면
+      _checkLoginStatus();
+    } else {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => PermissionWidget()));
+    }
+  }
+
+  // 로그인 상태 확인
+  Future<void> _checkLoginStatus() async {
+    final refreshToken = await secureStorage.read(key: 'refreshToken');
+    print('refreshToken--------$refreshToken');
+    if (refreshToken != null) {
+      final state = await tokenRefreash(refreshToken:refreshToken);
+      if(state){ // accessToken 재발급 받은 경우
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => defaultAccount()),
         );
-      } else { // 로그인된 회원이 없으면
+      }
+      else{ // accessToken 재발급 실패한 경우
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => signingUP()),
         );
       }
-      // FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-      //   if (user != null) { // 로그인된 User가 존재하면 account 화면으로
-      //     try{
-      //       String user_id = await UserInfoManage().getUserId() ?? '';
-      //       String account_id = await AccountInfoMange().getAccount_id(user_id:user_id);
-      //       if(account_id.length > 0){ // 계좌가 생성된 user면 Main계좌 화면으로
-      //         Navigator.push(context, MaterialPageRoute(builder: (context) => MainAccount()));
-      //       }else { // 계좌가 생성되지 않은 user면 계좌 만들기 화면으로
-      //         Navigator.push(context, MaterialPageRoute(builder: (context) => defaultAccount()));
-      //       }
-      //     }catch (e) { // 만약 user_id를 가져오거나 account_id를 불러오는데 문제가 생겼다면 아직 회원가입이 안되어 있어 계좌가 없는 유저일 가능성이 큼.
-      //       print(e);
-      //       Navigator.push(context,
-      //           MaterialPageRoute(builder: (context) => defaultAccount()));
-      //     }
-      //   }else{ // 로그인된 User가 없으면 로그인 화면으로
-      //     Navigator.push(context,
-      //         MaterialPageRoute(builder: (context) => signingUP()));
-      //   }
-      // });
-    } else {
+    } else { // 로그인 안되어 있는 경우
       Navigator.push(
-          context, MaterialPageRoute(builder: (context) => PermissionWidget()));
+        context,
+        MaterialPageRoute(builder: (context) => signingUP()),
+      );
     }
+  }
+
+  // json에서 값 가져오는 함수
+  Map<String, String?> parseTokens(String responseBody) {
+    final parsed = jsonDecode(responseBody);
+    return {
+      'access_token': parsed['access_token'],
+      'refresh_token': parsed['refresh_token'],
+    };
+  }
+
+  // refreash 토큰으로 카카오에 accesstoken 다시 요청하기 - 섹션관리
+  Future<bool> tokenRefreash({required String refreshToken}) async {
+    try { // 카카오에 Refresh 토큰으로 access token 요청하기
+      final value = await tokenRefreshPost(refreshToken: refreshToken);
+      if (value['statusCode'] == 200) { // 성공적으로 토큰을 받은 경우
+        Map<String, dynamic> tokens = parseTokens(value['body']);
+        // 새로운 토큰 저장
+        print('새로 받아온 카카오 토큰 --------- \n $tokens');
+        await secureStorage.write(key: 'refreshToken', value: tokens['refresh_token']);
+        await secureStorage.write(key: 'kakaoToken', value: tokens['access_token']);
+        try { // 우리 서버에 갱신한 토큰 보내기
+          final state = await tokenReissuePost(accessToken: tokens['access_token'], refreshToken: tokens['refresh_token']);
+          if (state['statusCode'] == 200) { // 서버에 문제없이 잘 도착한 경우
+            return true;
+          } else { // 서버에 문제가 있는 경우
+            print('서버에 문제가 있는 경우 --- $state');
+          }
+        } catch (e) {// 서버 요청 중 오류가 발생한 경우
+          print("tokenReissuePost 서버 요청 중 오류가 발생");
+          print(e);
+        }
+      } else { // Refresh token이 만료된 경우
+        print('Refresh token 만료, 재 로그인이 필요합니다.');
+      }
+    } catch (error) { // 요청 중 오류가 발생한 경우
+      print("tokenRefreashPost 서버 요청 중 오류가 발생");
+      print(error);
+    }
+    return false;
   }
 
   Widget build(BuildContext context) {
@@ -84,18 +119,20 @@ class _HomeState extends State<Home> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
-              child: Column(children: [
-                SizedBox(
-                  width: 250,
-                  height: 250,
-                  child: Image(
-                    image: AssetImage('timebank.png'),
-                  ),
+            child: Column(children: [
+              SizedBox(
+                width: 250,
+                height: 250,
+                child: Image(
+                  image: AssetImage('timebank.png'),
                 ),
-                SizedBox(
-                  height: screenHeight * 0.03,
-                ),
-              ])),
+              ),
+              SizedBox(
+                height: screenHeight * 0.03,
+              ),
+            ]
+            )
+          ),
         ],
       ),
     );
