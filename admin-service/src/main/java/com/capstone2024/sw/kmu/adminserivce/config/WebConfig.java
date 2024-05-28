b0c07084a1102641d1991f4ccbbdfae5d8c6f166
ytjdud <102743371+ytjdud@users.noreply.github.com>
2024-05-29 00:13:23 +0900

    Add allowedOrigin for localhost:58656 in CORS config to enable local admin page for TimeBank

---
+++ admin-service/src/main/java/com/capstone2024/sw/kmu/adminserivce/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.capstone2024.sw.kmu.adminserivce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/timebank-admin-service/api/**")
+                .allowedOrigins("http://localhost:58656")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
