1f965b10e15e806025d3d7d7705663df52c56b12
sunjung Oh <sspure1214@naver.com>
2024-05-31 12:16:44 +0900

    key 변경

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 import 'package:suntown/home/home.dart';
 
 void main() async {
